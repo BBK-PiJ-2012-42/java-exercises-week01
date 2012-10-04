13b8d781e457faea975a17602298b058ef751cc8
Tom Nealon <tgnealon@gmail.com>
2012-10-04 23:03:25 +0100

    Ex14

---
+++ Exercises.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercises;
+
+/**
+ *
+ * @author tom
+ */
+public class Exercises {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
---
+++ exercise14.java
@@ -0,0 +1,23 @@
+package exercises;
+
+public class exercise14 {
+    public static void main(String[] args) {
+        boolean prime;
+        boolean finished = false;
+        int primeCounter = 0;
+        for(int i=2; !finished; i++){
+            prime = true;
+            for(int j=(int) Math.sqrt(i); j>1; j--) {
+                if(i%j==0) {
+                    prime = false;
+                }
+            }
+            if(prime) {
+                System.out.println(i);
+                primeCounter++;
+                if(primeCounter==1000) { finished=true; }
+            }
+        }
+    }
+}
+
