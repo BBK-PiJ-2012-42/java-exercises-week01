5b86fee1fc84fafa817226947efc90f0a09c7e15
Tom Nealon <tgnealon@gmail.com>
2012-10-04 21:28:35 +0100

    First attempt at prime check exercise 13. Need to only check from root of number.

---
+++ exercise13.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercises;
+
+public class exercise13 {
+    public static void main(String[] args) {
+        boolean prime;
+        for(int i=2; i<1001; i++){
+            prime = true;
+            for(int j=i-1; j>1; j--) {
+                if(i%j==0) {
+                    prime = false;
+                }
+            }
+            if(prime) {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
