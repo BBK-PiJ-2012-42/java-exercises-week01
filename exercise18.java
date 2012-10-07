07f8e58041a113f7bbf2cdafee44acebde7a179e
Tom Nealon <tgnealon@gmail.com>
2012-10-07 18:22:56 +0100

    First attemt at ex18.

--- exercise18.java
+++ exercise18.java
@@ -4,14 +4,18 @@ import java.util.Scanner;
 
 public class exercise18 {
     public static void main(String[] args) {
-        int terms;
-        float roughPi = 0;
+        int terms, count;
+        double multiple, roughPi = 0, tenPi = 3.14159265358;
         Scanner input = new Scanner(System.in);
         System.out.print("Give me a number: ");
         terms = input.nextInt();
-        for(int j = 1; j == terms; j++) {
-            roughPi = roughPi + ( 4/j - 4/(j+2) );
-            System.out.print("With " + terms + " terms pi is " + roughPi);
+        count = 0;
+        for(int j = 1; j <= terms; j++) {
+            multiple = count * 4 + 1;
+            //System.out.println(multiple);
+            roughPi = roughPi + ( 4/multiple - 4/(multiple+2) );
+            System.out.println("With " + j + " terms pi is " + roughPi);
+            count++;
         }
     }
 }
